ce10a600778a5a97269f5ca587071652888614d9
YS-0911 <a9187049@gmail.com>
2025-09-26 00:51:48 +0900

    test13

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,6 @@
 // import dotenv from "dotenv";
 // dotenv.config();    
-const key = API_KEY; // 국립중앙도서관 API 키 입력
+const key = process.env.API_KEY; // 국립중앙도서관 API 키 입력
 
 // import dotenv from "dotenv";
 // dotenv.config();    
