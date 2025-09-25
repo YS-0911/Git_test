4aea43e3f38e7f9b87d24ffc002f90851f1a7d8c
YS-0911 <a9187049@gmail.com>
2025-09-26 00:32:00 +0900

    test5

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // import dotenv from "dotenv";
 // dotenv.config();    
 // const key = process.env.API_KEY; // 국립중앙도서관 API 키 입력
+document.getElementById("searchBtn").addEventListener("click", searchBooks);
 let abcd = [];
 
 // 검색 함수
